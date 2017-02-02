6be8bd941894f4e65cfb49c865ecae90b4c10e86
Becca Ricks <baricks@gmail.com>
2017-02-02 12:16:55 -0500

    Bot tweets quotes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Big thanks to Darius Kazemi's work on @MuseumBot for this code and inspiration. Find the github here: https://github.com/dariusk/museumbot
-
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
@@ -9,6 +8,7 @@ var inflection = require('inflection');
 var wordfilter = require('wordfilter');
 var conf = require('./config.js');
 var Twitter = require('node-twitter');
+var myTweetQuote;
 var twitterRestClient = new Twitter.RestClient(
   conf.consumer_key,
   conf.consumer_secret,
@@ -16,9 +16,13 @@ var twitterRestClient = new Twitter.RestClient(
   conf.access_token_secret
 );
 
-var baseUrl = 'http://www.metmuseum.org/api/collection/collectionlisting?department=14&offset=500&pageSize=0&sortBy=Relevance&sortOrder=asc&perPage=500&page=';
+// Base URL
 
-wordfilter.addWords(['Coin','Fragment','Cover']);
+var baseUrl = 'http://www.metmuseum.org/api/collection/collectionlisting?department=14&offset=100&&perPage=100&showOnly=highlights%7CwithImage&page=';
+
+wordfilter.addWords(['Coin','Fragment','Cover', "Bottle", "Allah"]);
+
+// Pick random item from the page
 
 Array.prototype.pick = function() {
   return this[Math.floor(Math.random()*this.length)];
@@ -32,14 +36,16 @@ Array.prototype.pickRemove = function() {
 function generate() {
   var dfd = new _.Deferred();
 
+// Make the request
+
 console.log('Going to request');
-  var url = baseUrl + Math.floor((Math.random() * 54) + 1);
-  console.log(url)
+  var url = baseUrl + Math.floor((Math.random() * 54) + 1); // Pick a random page
+  //console.log(url)
   request(url, function (error, response, body) {
-    console.log('reqed',error, response.statusCode);
+    //console.log('reqed',error, response.statusCode);
     if (!error && response.statusCode == 200) {
       var data = JSON.parse(body).results.pick();
-      console.log(data);
+      //console.log(data);
       var name = data.title;
       var date = data.date;
       var medium = data.medium;
@@ -50,7 +56,7 @@ console.log('Going to request');
       if (data.largeImage) {
         var stream = fs.createWriteStream('image.jpg');
         stream.on('close', function() {
-          console.log('done');
+          //console.log('done');
           dfd.resolve(name + ', ' + date + '. ' + thingUrl, bigImageUrl, '<a href="' + thingUrl + '">' + name + '</a><br><br>The Metropolitan Museum of Art');
         });
         var r = request(bigImageUrl).pipe(stream);
@@ -69,21 +75,54 @@ console.log('Going to request');
   return dfd.promise();
 }
 
+function strip() {
+
+  var authors = [];
+
+  var rumi_quote = fs.readFileSync('./quotes/rumi.txt').toString('utf-8').split("\n");
+  var hafiz_quote = fs.readFileSync('./quotes/hafiz.txt').toString('utf-8').split("\n");
+  var darwish_quote = fs.readFileSync('./quotes/darwish.txt').toString('utf-8').split("\n");
+  var ghazali_quote = fs.readFileSync('./quotes/al_ghazali.txt').toString('utf-8').split("\n");
+
+  authors.push(rumi_quote, hafiz_quote, darwish_quote, ghazali_quote);
+  var n = Math.floor((Math.random() * authors.length));
+  var pick_author = authors[n];
+
+  var m = Math.floor((Math.random() * pick_author.length));
+  var pick_quote = pick_author[m];
+
+  myTweetQuote = pick_quote;
+
+}
+
 function tweet() {
   // TODO Change this so that it alternates between tweeting different things
 
-  // 30 min - tweet quote
+  // create the tweet for quotes
+  strip();
 
-  // 30 min - tweet architecture
-  // TODO Find an API for architecture
+  // Tweet the quote
+  if (!wordfilter.blacklisted(myTweetQuote)) {
+    twitterRestClient.statusesUpdate({
+      'status': myTweetQuote
+    },
 
-  // 30 min - tweet quote
+    function(error, result) {
+      if (error) {
+        console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+      }
+      if (result) {
+        console.log("It worked");
+      }
+    });
+  }
 
-  // 30 min - tweet image
+  // Tweet the image
 
   generate().then(function(myTweet, bigImageUrl) {
     if (!wordfilter.blacklisted(myTweet)) {
-      console.log(myTweet);
+      //console.log(myTweet);
+
       twitterRestClient.statusesUpdateWithMedia({
         'status': myTweet,
         'media[]': 'image.jpg'
